d8b36490b4d1579e3f1af242621e27197db6ce9a
Dula Purkaystha <dpurkays@sfu.ca>
2020-12-22 23:16:28 -0800

    Basic UI set

--- app/src/main/java/ca/dpurkays/mycalc/MainActivity.java
+++ app/src/main/java/ca/dpurkays/mycalc/MainActivity.java
@@ -3,12 +3,43 @@ package ca.dpurkays.mycalc;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText input;
+    private Button zero, one, two, three, four, five, six, seven, eight, nine;
+    private Button plus, minus, divide, multiply, decimal, clear;
+    private int val1, val2;
+    private String op;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        input = (EditText) findViewById(R.id.userInput);
+
+        zero = (Button) findViewById(R.id.btn0);
+        one = (Button) findViewById(R.id.btn1);
+        two = (Button) findViewById(R.id.btn2);
+        three = (Button) findViewById(R.id.btn3);
+        four = (Button) findViewById(R.id.btn4);
+        five = (Button) findViewById(R.id.btn5);
+        six = (Button) findViewById(R.id.btn6);
+        seven = (Button) findViewById(R.id.btn7);
+        eight = (Button) findViewById(R.id.btn8);
+        nine = (Button) findViewById(R.id.btn9);
+
+        plus = (Button) findViewById(R.id.btnPlus);
+        minus = (Button) findViewById(R.id.btnMinus);
+        multiply = (Button) findViewById(R.id.btnMultiply);
+        divide = (Button) findViewById(R.id.btnDivide);
+        decimal = (Button) findViewById(R.id.btnDecimal);
+        clear = (Button) findViewById(R.id.btnClear);
+
+
+
     }
 }
